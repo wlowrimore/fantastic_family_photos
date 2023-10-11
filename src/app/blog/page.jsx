1afaca2fcd9034af3aade08969dd0751c130fe6c
William Lowrimore <wlowrimore@gmail.com>
2023-10-11 00:04:41 -0500

    adds blog/blogPost sets up basic mongodb

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+// import './blog.css';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='flex flex-col justify-center gap-10'>
+      <Link href='/blog/testId' className='flex gap-10 items-center'>
+        <div className=''>
+          <Image
+            src='https://images.pexels.com/photos/819370/pexels-photo-819370.jpeg?auto=compress&cs=tinysrgb&w=1600'
+            alt=''
+            width={400}
+            height={250}
+            className=''
+          />
+        </div>
+        <div className='content'>
+          <h1 className='title'>Test</h1>
+          <p className='desc'>Description</p>
+        </div>
+      </Link>
+      <Link href='/blog/testId' className='flex gap-10 items-center'>
+        <div className=''>
+          <Image
+            src='https://images.pexels.com/photos/819370/pexels-photo-819370.jpeg?auto=compress&cs=tinysrgb&w=1600'
+            alt=''
+            width={400}
+            height={250}
+            className=''
+          />
+        </div>
+        <div className='content'>
+          <h1 className='title'>Test</h1>
+          <p className='desc'>Description</p>
+        </div>
+      </Link>
+      <Link href='/blog/testId' className='flex gap-10 items-center'>
+        <div className=''>
+          <Image
+            src='https://images.pexels.com/photos/819370/pexels-photo-819370.jpeg?auto=compress&cs=tinysrgb&w=1600'
+            alt=''
+            width={400}
+            height={250}
+            className=''
+          />
+        </div>
+        <div className='content'>
+          <h1 className='title'>Test</h1>
+          <p className='desc'>Description</p>
+        </div>
+      </Link>
+    </div>
   )
 }
 
